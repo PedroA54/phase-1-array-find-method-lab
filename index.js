eeb9a3beb25ebf6720ed0d61ef4c772f462bea6a
Pedro Alarcon <pedroa54@icloud.com>
2024-02-03 22:05:25 -0600

    adds complete lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  
+  ]
+
+  function superbowlWin(record) {
+   
+    for (let i = 0; i < record.length; i++) {
+     
+      if (record[i].result === 'W') {
+        // Return the year if the Broncos won
+        return record[i].year;
+      }
+    }
+  
+    
+    return 'No wins';
+  }
+
+  function superbowlWin(record) {
+   
+    for (let i = 0; i < record.length; i++) {
+    
+      if (record[i].result === 'W') {
+     
+        return record[i].year;
+      }
+    }
+  
+    // Return undefined if no win object is found
+    return undefined;
+  }
